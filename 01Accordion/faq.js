3ed0c0710066364fdf357461166d66199a6659e1
Avijit826 <avijitdas826@gmail.com>
2022-10-22 10:45:58 +0530

    FAQ's Added

--- 01Accordion/faq.js
+++ 01Accordion/faq.js
@@ -18,19 +18,45 @@ const faqData = [
   },
 ];
 
-const accordianBody;
-const faqs = [];
+const accordianBody = document.querySelector(".accordian_body");
 
-function showFaq() {
-  
+let currentFaq = 0;
+while(currentFaq < faqData.length){
+  createFaq();
+  currentFaq++;
 }
 
 function createFaq() {
+  const currentFaqData = faqData[currentFaq]
+  const faq = document.createElement('div');
+accordianBody.appendChild(faq).className = 'faq'
+  const faqHeader = document.createElement('div');
+  faq.appendChild(faqHeader).className = 'faq_header'
+  let query = document.createElement('h3')
+  query.textContent = currentFaqData.question;
+  faqHeader.appendChild(query)
+  let showBtn = document.createElement('button')
+  showBtn.textContent = '+'
+  faqHeader.appendChild(showBtn).className= 'show_btn'
+  const faqAns = document.createElement('p')
+  faqAns.textContent = currentFaqData.answer;
+  faq.appendChild(faqAns).className='hidden';
   
 }
 
-function btnStatusUpdate() {
-  
-}
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach(faq =>{
+  faq.childNodes[0].childNodes[1].addEventListener('click',() => {
+    if (faq.childNodes[0].childNodes[1].innerHTML == '+'){
+      faq.childNodes[0].childNodes[1].innerHTML = '-';
+      faq.childNodes[1].style.display = 'block'
+    }
+    else{
+      faq.childNodes[0].childNodes[1].innerHTML = '+'
+      faq.childNodes[1].style.display = 'none'
+    }
+  })
+})
+
 
 
