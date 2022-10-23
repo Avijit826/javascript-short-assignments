e6c1013f94ed4312762620c5cea878067e5f8671
Avijit826 <avijitdas826@gmail.com>
2022-10-23 10:32:01 +0530

    joke modified with auto fetch

--- 06JokesGenerator - API/script.js
+++ 06JokesGenerator - API/script.js
@@ -2,8 +2,14 @@ const joke = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 const api = 'https://api.chucknorris.io/jokes/random'
 
+// calls API to get the joke on button click
 jokeBtn.addEventListener('click',() => {
     fetch(api).then(res => res.json()).then(data =>{
         joke.innerHTML = data.value
     })
 })
+
+// Fetch initial data when document loads
+window.onload = () =>{
+    jokeBtn.click()
+}
