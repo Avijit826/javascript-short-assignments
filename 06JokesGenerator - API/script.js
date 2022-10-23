ef0f842f031cd589905efbe598aee0e370e121e3
Avijit826 <avijitdas826@gmail.com>
2022-10-23 10:24:49 +0530

    joke modified

--- 06JokesGenerator - API/script.js
+++ 06JokesGenerator - API/script.js
@@ -2,7 +2,6 @@ const joke = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 const api = 'https://api.chucknorris.io/jokes/random'
 
-// let getJoke = 
 jokeBtn.addEventListener('click',() => {
     fetch(api).then(res => res.json()).then(data =>{
         joke.innerHTML = data.value
